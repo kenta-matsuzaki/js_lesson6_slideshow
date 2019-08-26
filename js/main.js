ccb41b79bf99fc9443af96c928cd997089492b51
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-26 13:54:47 +0900

    Summary(required)

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+{
+  const images = [
+    'img/pic00.jpg',
+    'img/pic01.jpg',
+    'img/pic02.jpg',
+    'img/pic03.jpg',
+    'img/pic04.jpg',
+    'img/pic05.jpg',
+    'img/pic06.jpg',
+  ];
+
+  let currentNum = 0;
+
+  document.querySelector('main img').src = images[currentNum];
+}
